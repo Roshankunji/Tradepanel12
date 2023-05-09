02f8ed299a36860542464130469064aeda1093f3
Roshan <roshan@kunji.io>
2023-05-09 15:55:49 +0530

    some changes added on dashboard page

--- pages/Dashboard.js
+++ pages/Dashboard.js
@@ -1,9 +1,31 @@
 import { Inter } from "next/font/google";
 import BaseCard from "../components/Molecules/Cards/BaseCard";
+import { useEffect, useState } from "react";
+import { ethers } from "ethers";
+import abi from "./abi.json";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Dashboard() {
+  const [name, setName] = useState();
+  // useEffect(async () => {
+  //   const addr = "0x824bef9c581f03ffd699b9bfdb9c714ac25f51b1";
+  //   const provider = new ethers.BrowserProvider(window.ethereum);
+  //   const contract = new ethers.Contract(addr, abi, provider);
+  //   const name = await contract.getBalances();
+  //   console.log("name is", name);
+  //   setName(name);
+  // }, []);
+
+  const connect = async () => {
+    const addr = "0x824bef9c581f03ffd699b9bfdb9c714ac25f51b1";
+    const provider = new ethers.BrowserProvider(window.ethereum);
+    const contract = new ethers.Contract(addr, abi, provider);
+    const name = await contract.getBalances();
+    console.log("name is", provider);
+    setName(name);
+  };
+
   return (
     <div className="flex flex-wrap min-h-screen bg-backgroundColor  py-10 justify-between ">
       <BaseCard
@@ -18,6 +40,7 @@ export default function Dashboard() {
         name="Funds Rollover"
         className="w-[100%] sm:w-[45%] md:w-[30%]"
       />
+      <button onClick={connect}>connect</button>
     </div>
   );
 }
